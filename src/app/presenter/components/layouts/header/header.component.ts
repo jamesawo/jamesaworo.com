e5ba8fa8eb319e8925da153a33f19d8ec18fcf2a
James Aworo <james.aworo@outlook.com>
2023-09-11 17:16:34 +0100

    update(pages): remove header from all page

--- src/app/presenter/components/layouts/header/header.component.ts
+++ src/app/presenter/components/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+    @Input()
+    showNavigation: boolean = true;
 }
