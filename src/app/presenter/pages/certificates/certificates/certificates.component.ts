24ad79ce4718aa99ed92aba476421bd153adc48b
James Aworo <james.aworo@outlook.com>
2023-10-05 02:52:30 +0100

    add certificate component

--- src/app/presenter/pages/certificates/certificates/certificates.component.ts
+++ src/app/presenter/pages/certificates/certificates/certificates.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Type} from '@angular/core';
+import {CertProps} from "../../../../core/types/types";
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom, shareReplay} from "rxjs";
+import {
+    CertCoursesInteractor
+} from "../../../../data/interactors/implementations/certifications/cert-courses.interactor";
+
+export type ComponentProps = { courses: CertProps[], certifications: CertProps[] };
 
 @Component({
-  selector: 'app-certificates',
-  templateUrl: './certificates.component.html',
-  styles: [
-  ]
+    selector: 'app-certificates',
+    templateUrl: './certificates.component.html',
+    styles: []
 })
-export class CertificatesComponent {
+export class CertificatesComponent implements OnInit {
+    public certAndCourses: ComponentProps = {courses: [], certifications: []};
 
+    constructor(private http: HttpClient, private interactor: CertCoursesInteractor) {
+    }
+
+    public async ngOnInit(): Promise<void> {
+        const result = this.interactor.getCertifications().pipe(shareReplay());
+        const values = await firstValueFrom(result);
+        if (values) {
+            this.certAndCourses = values;
+        }
+    }
 }
