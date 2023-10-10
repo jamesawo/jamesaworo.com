d136c312dbccda6412295915ab55c650110af62d
James Aworo <james.aworo@outlook.com>
2023-10-10 20:18:11 +0100

    update home page; add personal development link

--- src/app/core/types/types.ts
+++ src/app/core/types/types.ts
@@ -1,9 +1,25 @@
+export enum CertStatus {
+    COMPLETED = "completed",
+    PENDING = "pending",
+    IN_VIEW= "ongoing"
+}
 export interface Result {
     acknowledged: boolean;
     insertedId?: string;
 }
 
+export interface CertOrg {
+    name: string;
+    logo: string;
+}
+
 export interface CertProps {
     title: string;
     verificationUrl: string;
+    organization: CertOrg,
+    status: CertStatus,
+    courses: CertProps[],
+    date: string;
+    expiration: string;
+    skills: string[];
 }
