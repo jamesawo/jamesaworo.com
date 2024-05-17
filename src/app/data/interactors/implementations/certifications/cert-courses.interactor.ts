6d64c7e10352f0a73d7bce9f3eaac860bba7953b
James Aworo <james.aworo@outlook.com>
2024-05-17 18:12:45 +0100

    feat: use local json data

--- src/app/data/interactors/implementations/certifications/cert-courses.interactor.ts
+++ src/app/data/interactors/implementations/certifications/cert-courses.interactor.ts
@@ -1,15 +1,17 @@
 import {Injectable} from "@angular/core";
-import {IBookmarkInteractor} from "../../contracts/ibookmark.interactor";
 import {HttpClient} from "@angular/common/http";
 import {ComponentProps} from "../../../../presenter/pages/certificates/certificates/certificates.component";
 
 @Injectable({ providedIn: 'root' })
 export class CertCoursesInteractor {
 
+    public url = `https://raw.githubusercontent.com/jamesawo/mock/main/certs.json`;
+    public localCerts = '/assets/json/certs.json';
+
     constructor(private http: HttpClient) {
     }
 
     public getCertifications(){
-        return  this.http.get<ComponentProps>(`https://raw.githubusercontent.com/jamesawo/mock/main/certs.json`);
+        return  this.http.get<ComponentProps>(this.localCerts);
     }
 }
